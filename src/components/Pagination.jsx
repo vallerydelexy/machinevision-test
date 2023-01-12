0152f51755c9ef5837d2c6df168a1db682d475b7
rizki aprita <vallerydelexy@gmail.com>
2023-01-13 02:25:48 +0700

    fixed pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -9,10 +9,10 @@ import { Link } from "react-router-dom";
 export default function Pagination({ currentPage, totalPage, page }) {
 
   return (
-    <nav className="border-t border-gray-200 px-4 flex items-center sm:px-0 pb-4">
-      <div className="-mt-px w-0 flex-1 flex">
-        <a
-          href=""
+    <nav className="border-t border-gray-200 px-4 flex items-center sm:px-0 pb-4 mx-auto justify-center">
+      <div className="-mt-px  flex-0 flex">
+        {currentPage?<Link
+          to={`/?page=${currentPage-1}`}
           className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
         >
           <ArrowNarrowLeftIcon
@@ -20,7 +20,7 @@ export default function Pagination({ currentPage, totalPage, page }) {
             aria-hidden="true"
           />
           Previous
-        </a>
+        </Link>:null}
       </div>
       <div className="hidden md:-mt-px md:flex">
         {[...Array(totalPage)].map((_, i) => {
@@ -42,9 +42,9 @@ export default function Pagination({ currentPage, totalPage, page }) {
           }
         })}
       </div>
-      <div className="-mt-px w-0 flex-1 flex justify-end">
-        <a
-          href="#"
+      <div className="-mt-px flex-0 flex">
+        <Link
+          to={`/?page=${currentPage+1}`}
           className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
         >
           Next
@@ -52,7 +52,7 @@ export default function Pagination({ currentPage, totalPage, page }) {
             className="ml-3 h-5 w-5 text-gray-400"
             aria-hidden="true"
           />
-        </a>
+        </Link>
       </div>
     </nav>
   );
