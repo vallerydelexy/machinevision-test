24336c9f756c30a352054d2188474ef47bbc0786
rizki aprita <vallerydelexy@gmail.com>
2023-01-13 02:53:07 +0700

    fix current route color

--- src/components/staticSidebar.jsx
+++ src/components/staticSidebar.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
 import { classNames } from '../utils/index'
+import { useLocation } from "react-router-dom"
+import { useRecoilState } from 'recoil'
+import { navigationAtom } from '../recoil/navigationAtom'
+
 
 export default function StaticSidebar({navigation}){
-return <div className="hidden md:flex md:flex-shrink-0">
+  const location = useLocation()
+return (<div className="hidden md:flex md:flex-shrink-0">
 <div className="flex flex-col w-48">
-  {/* Sidebar component, swap this element with another sidebar if you like */}
   <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-white">
     <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
       <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
@@ -13,7 +17,7 @@ return <div className="hidden md:flex md:flex-shrink-0">
             key={item.name}
             to={item.href}
             className={classNames(
-              item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+              location?.pathname == item.href ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
               'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
             )}
           >
@@ -31,5 +35,5 @@ return <div className="hidden md:flex md:flex-shrink-0">
     </div>
   </div>
 </div>
-</div>
+</div>)
 }
